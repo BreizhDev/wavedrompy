82d452d2b6365110ecab3f71f0f9542fe266d148
wifasoi <wifasoi@gmail.com>
2018-11-29 08:52:59 +0100

    [WIP] copying the wavedrom implementation

--- wavedrom/reg.py
+++ wavedrom/reg.py
@@ -31,10 +31,40 @@ def labelArr(desc, opt):
     pass
 
 def lane(desc, opt):
-    pass
+    res = container.g(transfrom=Transform.translate(4.5,(opt.lanes - opt.index - 1) * opt.vspace + 0.5))
+    res.add(cage(desc, opt))
+    res.add(labels(desc, opt))
+    return res
+
+def vline(len,x=0 ,y=0):
+    return Line(start=(x,y), end=(x,y+len))
+
+def hline(len,x=0 ,y=0):
+    return Line(start=(x,y), end=(x+len,y))
 
 def cage(desc, opt):
-    pass
+    hspace = opt.hspace
+    vspace = opt.vspace
+    mod = opt.mod
+    res = container.g(stroke="black;stroke-width=1;stoke-linecap=round", transfrom=Transform.translate(0,vspace/4))
+    
+    res.add(hline(hspace))
+    res.add(vline(vspace/2))
+    res.add(hline(hspace, 0, vspace/2))
+    
+    i = opt.index*opt.mod
+    j = opt.mod
+    
+    for :
+        if j == opt.mod or :
+            res.add(vline(vspace/2, j*(hspace/mod)))
+        else:
+            res.add(vline(vlace/16),j*(hspace/mod))
+            res.add(vline(vlace/16),j*(hspace/mod), vspace*7/16)
+
+    return res
 
 def labels(desc, opt):
-    pass
+    ret = container.g(text-anchor=middle)
+    ret.add(labelArr(desc, opt))
+    return ret
--- wavedrom/wavedrom.py
+++ wavedrom/wavedrom.py
@@ -1165,6 +1165,64 @@ class WaveDrom(object):
 
         return svg_output
 
+    ############################à
+    def typeStyle(t=""):
+        style = {
+            "2":0
+            "3":80
+            "4":170
+            "5":45
+            "6":126
+            "7":215
+        }
+        if(t in style):
+            return ';fill:hsl({},100%,50%)'.format(style[t])
+        else:
+            return ''
+
+    def labelArr(desc, opt):
+        pass
+
+    def lane(desc, opt):
+        res = container.g(transfrom=Transform.translate(4.5,(opt.lanes - opt.index - 1) * opt.vspace + 0.5))
+        res.add(cage(desc, opt))
+        res.add(labels(desc, opt))
+        return res
+
+    def vline(len,x=0 ,y=0):
+        return Line(start=(x,y), end=(x,y+len))
+
+    def hline(len,x=0 ,y=0):
+        return Line(start=(x,y), end=(x+len,y))
+
+    def cage(desc, opt):
+        hspace = opt.hspace
+        vspace = opt.vspace
+        mod = opt.mod
+        res = container.g(stroke="black;stroke-width=1;stoke-linecap=round", transfrom=Transform.translate(0,vspace/4))
+        
+        res.add(hline(hspace))
+        res.add(vline(vspace/2))
+        res.add(hline(hspace, 0, vspace/2))
+        
+        i = opt.index*opt.mod
+        j = opt.mod
+        
+        for :
+            if j == opt.mod or :
+                res.add(vline(vspace/2, j*(hspace/mod)))
+            else:
+                res.add(vline(vlace/16),j*(hspace/mod))
+                res.add(vline(vlace/16),j*(hspace/mod), vspace*7/16)
+
+        return res
+
+    def labels(desc, opt):
+        ret = container.g(text-anchor=middle)
+        ret.add(labelArr(desc, opt))
+        return ret
+
+
 
 def main(args=None):
     if not args:
